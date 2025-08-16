f0eae46b312067ed3c1900e3f82d870ea09eb05f
gauravk1982 <gauravkundalia12@gmail.com>
2025-08-16 20:12:19 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import MP4Compressor from './MP4Compressor';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <MP4Compressor />
+  </React.StrictMode>
+);
